aa1c29f8f47f810192a48a0cb5b60e22caa2254f
ibunx17 <iqbal19847@gmail.com>
2025-05-18 17:51:38 +0700

    fix new password

--- event-order-app/front-end/src/app/new-password/page.tsx
+++ event-order-app/front-end/src/app/new-password/page.tsx
@@ -1,5 +1,9 @@
 import NewPasswordPage from "@/components/new-password";
+import { Suspense } from "react";
 
 export default function NewPassword() {
-  return <NewPasswordPage />;
+  return (<Suspense>
+          <NewPasswordPage />;
+        </Suspense>
+  )
 }
